b5d705d2b13afe806c6277d1f94eee132799a39c
gjlaurent <guillaume.laurent@ens2m.fr>
2023-06-14 11:17:48 +0200

    New example

---
+++ examples/analysingPatternFromFile.cpp
@@ -0,0 +1,24 @@
+#include "Vernier.hpp"
+
+using namespace Vernier;
+using namespace cv;
+using namespace std;
+using namespace Eigen;
+
+/** This example analyse the spectrum of a pattern. 
+ */
+int main() {
+
+    // Loading the image
+    Mat image = cv::imread("megarena.png");
+    
+    // Analysing the spectrum
+    ArrayXXd array = image2array(image);
+    PatternPhase patternPhase;
+    patternPhase.compute(array);
+    
+    // Showing image and its spectrum
+    imshow("Found peaks (red = dir 1, green = dir 2)", patternPhase.getPeaksImage());
+    imshow("Phase fringes (red = dir 1, green = dir 2)", patternPhase.getFringesImage());
+    waitKey();
+}
