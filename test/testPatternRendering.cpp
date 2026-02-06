8ec49517115c0c98179554ea42f29ed0e52c5bdb
gjlaurent <guillaume.laurent@ens2m.fr>
2026-02-06 15:37:30 +0100

    Good parameters

--- test/testPatternRendering.cpp
+++ test/testPatternRendering.cpp
@@ -14,16 +14,17 @@ using namespace std;
 void mainOrthographic2d() {
 
     // Constructing the layout
-    double physicalPeriod = 1.0;
+    double physicalPeriod = 10.0;
     int codeSize = 12;
-    PatternLayout* layout = new MegarenaPatternLayout(physicalPeriod, codeSize);
+    //PatternLayout* layout = new MegarenaPatternLayout(physicalPeriod, codeSize);
+    PatternLayout* layout = new PeriodicPatternLayout(physicalPeriod, 11,21);
     cout << "  Code size: " << codeSize << endl;
     cout << "  Physical period: " << physicalPeriod << endl;
 
     // Setting the pose of the pattern in the camera frame for rendering
     double x = 0.0;
     double y = 0.0;
-    double alpha = 0.0;
+    double alpha = 0.2;
     double pixelSize = 0.5;
     Pose patternPose = Pose(x, y, alpha, pixelSize);
     cout << "  Pattern pose:   " << patternPose.toString() << endl;
@@ -49,11 +50,11 @@ void mainOrthographic3d() {
     // Setting the pose of the pattern in the camera frame for rendering
     double x = -200 * 15.0;
     double y = -300 * 15.0;
-    double z = 5000.0;
+    double z = 0.0;
     double alpha = -0.2;
     double beta = 1.10;
     double gamma = 0.05;
-    double pixelSize = 2.0;
+    double pixelSize = 0.5;
     Pose patternPose = Pose(x, y, z, alpha, beta, gamma, pixelSize);
     cout << "  Pattern pose:     " << patternPose.toString() << endl;
 
@@ -77,12 +78,12 @@ void mainPerspective() {
 
     // Setting the pose of the pattern in the camera frame for rendering
     double x = 0.0;
-    double y = 0.0;
-    double z = 2500.0;
+    double y = -100.0;
+    double z = 300.0;
     double alpha = 0.0;
-    double beta = 0.0;
+    double beta = 0.5;
     double gamma = 0.0;
-    double pixelSize = 0.5;
+    double pixelSize = 0.002;
     Pose patternPose = Pose(x, y, z, alpha, beta, gamma, pixelSize);
     cout << "  Pattern pose:     " << patternPose.toString() << endl;
 
@@ -90,7 +91,7 @@ void mainPerspective() {
     cv::Mat image(512, 512, CV_64F);
     //layout->renderOrthographicProjection(patternPose, image);
     
-    double focalLength = 5000; 
+    double focalLength = 6; 
     layout->renderPerspectiveProjection(patternPose, image, focalLength);
 
     // Showing the image
